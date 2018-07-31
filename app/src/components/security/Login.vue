3ae3f364ec5fbb930a9a6871c05de29f2cff6a69
Emiliano Viada <emjovi@gmail.com>
2018-07-30 23:37:31 -0300

    Fixed error when bad credentials are given and then we want to login again

--- app/src/components/security/Login.vue
+++ app/src/components/security/Login.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="login bg-white rounded px-3 py-3 mx-auto mt-5">
     <h1 class="text-center">Inmo Web</h1>
-    <form class="mt-3">
+    <form @submit.prevent="onSubmit" class="mt-3">
       <div class="form-group">
         <label for="email">Direcci&oacute;n de E-mail</label>
-        <input type="email" v-validate="'required|email'" :class="{'form-control': true, 'is-danger': errors.has('email') }" id="email" name="email" v-model="email" aria-describedby="emailHelp" placeholder="Ingresa tu email">
+        <input type="email" :class="{'form-control': true, 'is-danger': errors.has('email') }" id="email" name="email" v-model="email" aria-describedby="emailHelp" placeholder="Ingresa tu email" v-validate="'required|email'">
         <span v-show="errors.has('email')" class="help is-danger">{{ errors.first('email') }}</span>
       </div>
       <div class="form-group">
         <label for="password">Contrase&ntilde;a</label>
-        <input type="password" v-validate="'required|min:6'" :class="{'form-control': true, 'is-danger': errors.has('password') }" v-model="password" id="password" name="password" placeholder="Contraseña">
+        <input type="password" :class="{'form-control': true, 'is-danger': errors.has('password') }" v-model="password" id="password" name="password" placeholder="Contraseña" v-validate="'required|min:6'" @focus="pwdOnFocus">
         <span v-show="errors.has('password')" class="help is-danger">{{ errors.first('password') }}</span>
       </div>
       <div class="form-group text-right">
-        <button type="submit" v-on:click.prevent="onSubmit" :disabled="errors.any() || submitting" class="btn btn-info">
+        <button type="submit" :disabled="errors.any() || submitting" class="btn btn-info">
           Login
         </button>
       </div>
@@ -56,6 +56,9 @@ export default {
       this.loader = this.$loading.show()
       this.submitting = true
       this.validateBeforeSubmit()
+    },
+    pwdOnFocus () {
+      this.$validator.errors.clear()
     }
   }
 }
